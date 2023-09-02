d6bf5b713383f66a9e696bc9059d30af7fbf167f
rawat.rajeev <rawat.rajeev@orangemantra.in>
2023-09-02 18:28:56 +0530

    login screen

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react'
+
+import { TouchableOpacity, StyleSheet, View } from 'react-native'
+import { Text } from 'react-native-paper'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import TextInput from '../components/TextInput'
+import BackButton from '../components/BackButton'
+import { theme } from '../core/theme'
+import { emailValidator } from '../helpers/emailValidator'
+import { passwordValidator } from '../helpers/passwordValidator'
+import axios from 'axios'
+
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState({ value: '', error: '' })
+  const [password, setPassword] = useState({ value: '', error: '' })
+  const [error, setError] = useState()
+  const onLoginPressed = () => {
+    const emailError = emailValidator(email.value)
+    const passwordError = passwordValidator(password.value)
+    if (emailError || passwordError) {
+      setEmail({ ...email, error: emailError })
+      setPassword({ ...password, error: passwordError })
+      return
+    }
+    axios
+      .post('http://om-ev.1hour.in:3006/login', {
+        email: email.value,
+        password: password.value,
+      })
+      .then((res) => {
+        console.log(res)
+        if (res?.data?.token) {
+          navigation.navigate({
+            index: 0,
+            routes: [{ name: 'Dashboard' }],
+          })
+        } else {
+          setError(res)
+          console.log(res)
+        }
+      })
+      .catch((err) => {
+        setError(err.response.data.message)
+      })
+  }
+
+  return (
+    <Background>
+      <BackButton goBack={navigation.goBack} />
+      <Logo />
+      <Header>Welcome back.</Header>
+      <TextInput
+        label="Email"
+        returnKeyType="next"
+        value={email.value}
+        onChangeText={(text) => setEmail({ value: text, error: '' })}
+        error={!!email.error}
+        errorText={email.error}
+        autoCapitalize="none"
+        autoCompleteType="email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+      />
+      <TextInput
+        label="Password"
+        returnKeyType="done"
+        value={password.value}
+        onChangeText={(text) => setPassword({ value: text, error: '' })}
+        error={!!password.error}
+        errorText={password.error}
+        secureTextEntry
+      />
+      {error ? <span style={{ color: 'red' }}>{error}</span> : ''}
+      <Button mode="contained" onPress={onLoginPressed}>
+        Login
+      </Button>
+    </Background>
+  )
+}
+
+const styles = StyleSheet.create({
+  forgotPassword: {
+    width: '100%',
+    alignItems: 'flex-end',
+    marginBottom: 24,
+  },
+  row: {
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  forgot: {
+    fontSize: 13,
+    color: theme.colors.secondary,
+  },
+  link: {
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+  },
+})
---
+++ src/screens/StartScreen.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Paragraph from '../components/Paragraph'
+
+export default function StartScreen({ navigation }) {
+  return (
+    <Background>
+      <Logo />
+      <Header>Login Template</Header>
+      <Paragraph>
+        The easiest way to start with your amazing application.
+      </Paragraph>
+      <Button
+        mode="contained"
+        onPress={() => navigation.navigate('LoginScreen')}
+      >
+        Login
+      </Button>
+    </Background>
+  )
+}
